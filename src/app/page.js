b10ea544bb878c2c4b231b9d7b9b66d0f37f96c2
sandrbuz <Sandrbuz@mail.ru>
2025-07-11 16:34:55 +0300

    add base component styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="w-[1256px]  border border-[#E8EBF0] bg-white rounded-2xl py-10 px-16">
+      <div className="bg-gray-100 min-h-[100px]  ">
+        {/* Контент Home страницы */}
+      </div>
+    </div>
+  );
 }
