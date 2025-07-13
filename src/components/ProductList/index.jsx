1ce79bbc6c5e8662df5d0deaddc07fa1d1d22a94
sandrbuz <Sandrbuz@mail.ru>
2025-07-13 15:00:29 +0300

    feat: made a Show more button

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -6,6 +6,11 @@ export default function ProductList() {
     <div className="mt-4">
       <ProductListToolbar />
       <ProductsTable />
+      <button className="mt-[15px] w-full flex h-10 py-1 px-3 justify-center items-center gap-1.5 self-stretch rounded-lg border border-[#E8EBF0] hover:bg-[#FAFAFA]">
+        <span className="text-[#36394A] text-center font-[Inter] text-base font-medium leading-6">
+          Показать больше предложений
+        </span>
+      </button>
     </div>
   );
 }
